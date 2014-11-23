7ea6959e5dc3d072a5316814c0c58bad46347e9c
Luke Stanbery <lukestanbery89@gmail.com>
2014-11-23 16:13:37 -0600

    Built UI. Still working on back end functionality.

--- StrikethroughCleaner/Form1.Designer.cs
+++ StrikethroughCleaner/Form1.Designer.cs
@@ -23,12 +23,72 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxFileLocation = new System.Windows.Forms.TextBox();
+            this.buttonSelectExcelWorksheet = new System.Windows.Forms.Button();
+            this.buttonClean = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxFileLocation
+            // 
+            this.textBoxFileLocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxFileLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFileLocation.Location = new System.Drawing.Point(13, 13);
+            this.textBoxFileLocation.Name = "textBoxFileLocation";
+            this.textBoxFileLocation.ReadOnly = true;
+            this.textBoxFileLocation.Size = new System.Drawing.Size(393, 23);
+            this.textBoxFileLocation.TabIndex = 0;
+            // 
+            // buttonSelectExcelWorksheet
+            // 
+            this.buttonSelectExcelWorksheet.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonSelectExcelWorksheet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSelectExcelWorksheet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSelectExcelWorksheet.Location = new System.Drawing.Point(13, 42);
+            this.buttonSelectExcelWorksheet.Name = "buttonSelectExcelWorksheet";
+            this.buttonSelectExcelWorksheet.Size = new System.Drawing.Size(167, 30);
+            this.buttonSelectExcelWorksheet.TabIndex = 1;
+            this.buttonSelectExcelWorksheet.Text = "Select Excel Worksheet";
+            this.buttonSelectExcelWorksheet.UseVisualStyleBackColor = false;
+            this.buttonSelectExcelWorksheet.Click += new System.EventHandler(this.buttonSelectExcelWorksheet_Click);
+            // 
+            // buttonClean
+            // 
+            this.buttonClean.Enabled = false;
+            this.buttonClean.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClean.Location = new System.Drawing.Point(186, 42);
+            this.buttonClean.Name = "buttonClean";
+            this.buttonClean.Size = new System.Drawing.Size(220, 77);
+            this.buttonClean.TabIndex = 2;
+            this.buttonClean.Text = "Clean Strikethrough Characters";
+            this.buttonClean.UseVisualStyleBackColor = true;
+            this.buttonClean.Click += new System.EventHandler(this.buttonClean_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(418, 129);
+            this.Controls.Add(this.buttonClean);
+            this.Controls.Add(this.buttonSelectExcelWorksheet);
+            this.Controls.Add(this.textBoxFileLocation);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Strikethrough Cleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxFileLocation;
+        private System.Windows.Forms.Button buttonSelectExcelWorksheet;
+        private System.Windows.Forms.Button buttonClean;
     }
 }
 
--- StrikethroughCleaner/Form1.cs
+++ StrikethroughCleaner/Form1.cs
@@ -7,11 +7,73 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
 
 namespace StrikethroughCleaner {
     public partial class Form1:Form {
+
+        private string fileLocation;
+        Microsoft.Office.Interop.Excel.Application app;
+        Workbook workbook;
+
         public Form1() {
             InitializeComponent();
+        }
+
+        private void buttonSelectExcelWorksheet_Click(object sender, EventArgs e) {
+            using(OpenFileDialog dialog = new OpenFileDialog()) {
+                dialog.Filter = "Excel Worksheets (*.xls,*.xlsx)|*.xls;*.xlsx";
+                DialogResult result = dialog.ShowDialog();
+                if(result == DialogResult.OK) {
+                    fileLocation = dialog.FileName;
+                    textBoxFileLocation.Text = fileLocation;
+                    buttonClean.Enabled = true;
+                    buttonClean.BackColor = Color.LimeGreen;
+                }
+            }
+        }
+
+        private void buttonClean_Click(object sender, EventArgs e) {
+            if(fileLocation != null) {
+                app = new Microsoft.Office.Interop.Excel.Application();
+                workbook = app.Workbooks.Open(fileLocation);
+                Worksheet worksheet = (Worksheet)workbook.Worksheets.get_Item(1);
+                Range range = worksheet.UsedRange;
+
+                for(int rCnt = 1; rCnt <= range.Rows.Count; rCnt++) {
+                    for(int cCnt = 1; cCnt <= range.Columns.Count; cCnt++) {
+                        string s = "";
+                        int char_index = 1;
+                        int length = range.Cells.Value2.ToString().Length;
+
+                        while(char_index < length) {
+                            //just keep building the string with any char that is not strikethrough               
+                            if(!(bool)range.Cells.get_Characters(char_index, 1).Font.Strikethrough) {
+                                s += range.Cells.get_Characters(char_index, 1).Text;
+                            }
+                            char_index++;
+                        }
+                    }
+                }
+            }
+
+            using(SaveFileDialog saveDialog = new SaveFileDialog()) {
+                saveDialog.Filter = "Excel Workbook (*.xls)|*.xls";
+                DialogResult saveResult = saveDialog.ShowDialog();
+                if(saveResult == DialogResult.OK) {
+                    fileLocation = saveDialog.FileName;
+                    workbook.SaveAs(fileLocation);
+                }
+            }
+        }
+
+        ~Form1() {
+            if(app != null) {
+                if(workbook != null) {
+                    workbook.Close();
+                }
+                app.Quit();
+            }
         }
     }
 }
